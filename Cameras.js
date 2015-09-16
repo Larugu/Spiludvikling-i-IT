6db0c1c24a9e41de648816f229ce282fcc35f711
runeras <rr@1q.dk>
2015-09-16 13:16:49 +0200

    Update to Camra

--- Cameras.js
+++ Cameras.js
@@ -2,10 +2,12 @@
 
 var cam1 : Camera;
 var cam2 : Camera;
+var cam3 : Camera;
 
 function Start () {
 	cam1.enabled = true;
-    cam2.enabled = false;
+   	cam3.enabled = false; 
+   	cam2.enabled = false;
 }
 
 function Update () {
